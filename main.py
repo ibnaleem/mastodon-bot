355a72d7b9e504d1a8c7d48d709663ac655ae97d
ibnaleem <ibnaleem@outlook.com>
2023-05-29 00:34:09 +0100

    added check followers and following logic to determine unfollows; removed db

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 import json
 from mastodon import Mastodon
-from pymongo import MongoClient
-from pymongo.server_api import ServerApi
 
 with open("config.json", "r") as f:
     config = json.load(f)
 
 client = Mastodon(access_token = str(config["token"]), api_base_url="https://mastodon.social/")
-mongoClient = MongoClient(str(config["uri"]), server_api=ServerApi('1'))
-db = mongoClient.mastodon_db
-followers_collection = db.followers
 
 def check_followers(func):
     def wrapper(self, *args, **kwargs):
@@ -22,54 +17,26 @@ def check_followers(func):
                 # User has followed you
                 follower_id = notification['account']['id']
                 kwargs['follower_id'] = follower_id
-                store_follower_id(follower_id=follower_id)
                 break
 
         return func(self, *args, **kwargs)
 
     return wrapper
 
-def store_follower_id(follower_id):
-    doc = followers_collection.find_one({"account": int(follower_id)})
-
-    if doc is None:
-        followers_collection.insert_one({"account": int(follower_id)})
-    pass
-
 def check_unfollowers(func):
     def wrapper(self, *args, **kwargs):
-        follower_ids = retrieve_follower_ids()
-        if follower_ids:
-            for follower_id in follower_ids:
-                follower = self.account_followers(follower_id)
-                if not follower:
-                    following = self.account_following(follower_id)
+        following_list = self.account_following()
+        followers_set = set(follower["id"] for follower in self.account_followers())
 
-                    if following:
-                        kwargs['follower_id'] = follower_id
-                        remove_follower_id(follower_id)
-                        break
-                    pass
-        else:
+        for following in following_list:
+            if following["id"] not in followers_set:
+                kwargs['following_id'] = following["id"]
+                break
             pass
 
         return func(self, *args, **kwargs)
 
     return wrapper
-
-def retrieve_follower_ids():
-    cursor = followers_collection.find({}, {"account": 1})
-    
-    ids = [doc["account"] for doc in cursor]
-    return ids
-
-
-def remove_follower_id(follower_id):
-    doc = followers_collection.find_one({"account": int(follower_id)})
-
-    if doc is not None:
-        followers_collection.delete_one({"account": int(follower_id)})
-    pass
 
 class MyClient:
     def __init__(self, client):
@@ -89,10 +56,10 @@ class MyClient:
             follower_id = None
 
     @check_unfollowers
-    def unfollow(self, follower_id=None):
-        while follower_id:
-            self.account_unfollow(id=follower_id)
-            follower_id = None 
+    def unfollow(self, following_id=None):
+        while following_id:
+            self.account_unfollow(id=following_id)
+            following_id = None 
 
 
 if __name__ == "__main__":
