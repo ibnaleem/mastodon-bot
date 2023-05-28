14cadcbc927fada152360b5dfb1de0b808641c6e
ibnaleem <ibnaleem@outlook.com>
2023-05-28 21:27:55 +0100

    Using MySQL database, logs followers and unfollows if they unfollow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import json, sqlite3
 from mastodon import Mastodon
 
 with open("config.json", "r") as f:
@@ -16,16 +16,66 @@ def check_followers(func):
                 # User has followed you
                 follower_id = notification['account']['id']
                 kwargs['follower_id'] = follower_id
-                with open("followers.txt", "w") as f:
-                    f.write(f"{follower_id}\n")
-                    f.close()
                 break
 
         return func(self, *args, **kwargs)
 
     return wrapper
 
-# CHECK IF THE FOLLOWER_IDs IN THE FOLLOWERS.TXT IS IN THE FOLLOWERS OF ACCOUNT, IF NOT, UNFOLLOW THAT ACCOUNT
+def store_follower_id(follower_id):
+    # Connect to the SQLite database
+    conn = sqlite3.connect('followers.db')
+    cursor = conn.cursor()
+
+    # Create a table if it doesn't exist
+    cursor.execute('''CREATE TABLE IF NOT EXISTS followers
+                      (id INTEGER PRIMARY KEY)''')
+
+    # Insert the follower_id into the table
+    cursor.execute("INSERT INTO followers (id) VALUES (?)", (follower_id,))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+def check_unfollowers(func):
+    def wrapper(self, *args, **kwargs):
+
+        follower_ids = retrieve_follower_ids()
+        for follower_id in follower_ids:
+            follower = self.account_followers(follower_id)
+            if not follower:
+                following = self.account_following(follower_id)
+
+                if following:
+                    kwargs['follower_id'] = follower_id
+                    break
+
+                pass
+
+        return func(self, *args, **kwargs)
+
+    return wrapper
+
+def retrieve_follower_ids():
+    # Connect to the SQLite database
+    conn = sqlite3.connect('followers.db')
+    cursor = conn.cursor()
+
+    # Retrieve all follower IDs from the table
+    cursor.execute("SELECT id FROM followers")
+    follower_ids = cursor.fetchall()
+
+    # Close the connection
+    conn.close()
+
+    # Return the follower IDs as a list
+    return [follower_id[0] for follower_id in follower_ids]
+
+# - Store IDs in db
+# - Check if the IDs in the Db are found in followers
+# - Not found? Check following if ID is there
+# - Found? Unfollow, else return
 
 class MyClient:
     def __init__(self, client):
@@ -41,6 +91,12 @@ class MyClient:
     def follow(self, follower_id=None):
         while follower_id:
             self.account_follow(id=follower_id)
+            follower_id = None
+
+    @check_unfollowers
+    def unfollow(self, follower_id=None):
+        while follower_id:
+            self.account_unfollow(id=follower_id)
             follower_id = None 
 
 
@@ -50,3 +106,4 @@ if __name__ == "__main__":
 
     while True:
         MyClient.follow(client)
+        MyClient.unfollow(client)
