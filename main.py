c0a77bdd4a5507218888ee1d5325c66c5272c274
ibnaleem <ibnaleem@outlook.com>
2023-05-28 20:13:48 +0100

    added check_followers decorator

--- main.py
+++ main.py
@@ -15,6 +15,23 @@ class MyClient:
             print("LOGGED INTO MASTODON")
         else:
             print("FAILED TO LOGIN TO MASTODON")
+    
+    def check_followers(self, func):
+        def wrapper(*args, **kwargs):
+            # Fetch notifications for the logged-in user
+            notifications = self.notifications()
+
+            # Iterate through the notifications to check for follow events
+            for notification in notifications:
+                if notification['type'] == 'follow':
+                    # User has followed you
+                    follower_id = notification['account']['id']
+                    follower_username = notification['account']['username']
+                    func(follower_id, follower_username, *args, **kwargs)
+                    break
+
+        return wrapper
+
 
 if __name__ == "__main__":
     MyClient.login_check(client)
